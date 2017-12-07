31afc246f98741a99b94e67b508e4079dfb7f780
diemarc <diemarc.os@gmail.com>
2017-12-07 05:06:16 +0100

    refactor

--- config/conf.php
+++ config/conf.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ * This file is part of keranaProject
+ * Copyright (C) 2017-2018  diemarc  diemarc@protonmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace kerana;
 
@@ -156,8 +173,8 @@ define('_ENABLE_PROFILER_', true);
  */
 
 $config->set('_dbhost_', 'localhost');
-$config->set('_dbname_', 'kerana');
-$config->set('_dbuser_', 'oper_ker');
-$config->set('_dbpass_', 'darksky');
+$config->set('_dbname_', '');
+$config->set('_dbuser_', '');
+$config->set('_dbpass_', '');
 $config->set('_dbport_', '3306');
 
