475fbb73b6bf3fb92d5cc6c9caee2c68f24dc95a
diemarc <diemarc.os@gmail.com>
2017-12-13 10:16:06 +0100

    kerana refactor keranaForm

--- application/modules/system/view/users/index.php
+++ application/modules/system/view/users/index.php
@@ -1,40 +1,48 @@
 <div id="page-wrapper">
-    <h2>
-        <i class='fa fa-users'></i> System/Users</h2>
+    <h2>system/index</h2>
     <header class="breadcrumb">
-        <a href="<?php echo __URL__;?>/system/user/add" 
+        <a href="http://local.keranaproject/system/user/add" 
            class="btn btn-success">Add new</a>
     </header>
-    <?php echo \helpers\Url::getModule();?>
     <section id='results' class='table-responsive'>
-        <table class="table table-bordered table-condensed table-hover">
-            <thead class='bg-warning'>
-                <tr>
-                    <th>id_usuario</th> 
-                    <th>username</th> 
-                    <th>email</th> 
-                    <th>nombres</th> 
-                    <th>apellidos</th> 
-                    <th>sw_activo</th> 
-                    <th>Options</th> 
+      <table class="table table-bordered table-condensed table-hover">
+        <thead class='bg-warning'>
+            <tr>
+                <th>id_usuario</th> 
+<th>username</th> 
+<th>password</th> 
+<th>salt</th> 
+<th>email</th> 
+<th>nombres</th> 
+<th>apellidos</th> 
+<th>sw_activo</th> 
+<th>Options</th> 
 
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($rsUsuarios AS $usuario): ?>
-                    <tr> 
-                        <td><?php echo $usuario->id_usuario; ?></td> 
-                        <td><?php echo $usuario->username; ?></td> 
-                        <td><?php echo $usuario->email; ?></td> 
-                        <td><?php echo $usuario->nombres; ?></td> 
-                        <td><?php echo $usuario->apellidos; ?></td> 
-                        <td><?php echo $usuario->sw_activo; ?></td> 
-                        <td> 
-                            <a href="<?php echo __URL__;?>/system/user/edit/<?php echo $usuario->id_usuario;?>">Editar</a>
-                        </td> 
-                    </tr> 
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($rsUsers AS $user):?>
+<tr> 
+<td><?php echo $user->id_usuario; ?></td> 
+<td><?php echo $user->username; ?></td> 
+<td><?php echo $user->password; ?></td> 
+<td><?php echo $user->salt; ?></td> 
+<td><?php echo $user->email; ?></td> 
+<td><?php echo $user->nombres; ?></td> 
+<td><?php echo $user->apellidos; ?></td> 
+<td><?php echo $user->sw_activo; ?></td> 
+<td> 
+ <a href='/system/user/edit/<?php echo $user->id_usuario; ?>' 
+ class='btn btn-default btn-xs' title='Edit'>
+<i class='fa fa-edit'></i>
+</a> 
+<a href='/system/user/delete/<?php echo $user->id_usuario; ?>' 
+ class='btn btn-danger btn-xs' title='Delete'>
+<i class='fa fa-trash'></i></a> 
+ </td> 
+</tr> 
+<?php endforeach;?>
+        </tbody>
+      </table>
     </section>
 </div>
