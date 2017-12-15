5aaf30d76fb870d7076aaa30ef2c4ea609e229b4
diemarc <diemarc.os@gmail.com>
2017-12-15 06:14:31 +0100

    Restyling user index.

--- application/modules/system/view/users/index.php
+++ application/modules/system/view/users/index.php
@@ -1,48 +1,47 @@
 <div id="page-wrapper">
-    <h2>system/index</h2>
+    <h4 class='text-primary'> 
+        <i class="fa fa-users fa-2x"></i>
+        <span class='text-muted'>system</span>Users
+    </h4>
     <header class="breadcrumb">
         <a href="http://local.keranaproject/system/user/add" 
-           class="btn btn-success">Add new</a>
+           class="btn btn-default btn-sm">
+            <i class='fa fa-plus'></i>
+        </a>
     </header>
     <section id='results' class='table-responsive'>
-      <table class="table table-bordered table-condensed table-hover">
-        <thead class='bg-warning'>
-            <tr>
-                <th>id_usuario</th> 
-<th>username</th> 
-<th>password</th> 
-<th>salt</th> 
-<th>email</th> 
-<th>nombres</th> 
-<th>apellidos</th> 
-<th>sw_activo</th> 
-<th>Options</th> 
+        <table class="table table-bordered table-condensed table-hover">
+            <thead class='bg-info'>
+                <tr>
+                    <th>ID</th> 
+                    <th>Username</th> 
+                    <th>Email</th> 
+                    <th>Name</th> 
+                    <th>Is active?</th> 
+                    <th class='bg-primary'>Options</th> 
 
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($rsUsers AS $user):?>
-<tr> 
-<td><?php echo $user->id_usuario; ?></td> 
-<td><?php echo $user->username; ?></td> 
-<td><?php echo $user->password; ?></td> 
-<td><?php echo $user->salt; ?></td> 
-<td><?php echo $user->email; ?></td> 
-<td><?php echo $user->nombres; ?></td> 
-<td><?php echo $user->apellidos; ?></td> 
-<td><?php echo $user->sw_activo; ?></td> 
-<td> 
- <a href='/system/user/edit/<?php echo $user->id_usuario; ?>' 
- class='btn btn-default btn-xs' title='Edit'>
-<i class='fa fa-edit'></i>
-</a> 
-<a href='/system/user/delete/<?php echo $user->id_usuario; ?>' 
- class='btn btn-danger btn-xs' title='Delete'>
-<i class='fa fa-trash'></i></a> 
- </td> 
-</tr> 
-<?php endforeach;?>
-        </tbody>
-      </table>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($rsUsers AS $user): ?>
+                    <tr> 
+                        <td><?php echo $user->id_usuario; ?></td> 
+                        <td><?php echo $user->username; ?></td> 
+                        <td><?php echo $user->email; ?></td> 
+                        <td><?php echo $user->nombres . ' ' . $user->apellidos; ?></td> 
+                        <td><?php echo $user->sw_activo; ?></td> 
+                        <td class='well'> 
+                            <a href='/system/user/edit/<?php echo $user->id_usuario; ?>' 
+                               class='btn btn-default btn-xs' title='Edit'>
+                                <i class='fa fa-edit'></i>
+                            </a> 
+                            <a href='/system/user/delete/<?php echo $user->id_usuario; ?>' 
+                               class='btn btn-danger btn-xs' title='Delete'>
+                                <i class='fa fa-trash'></i></a> 
+                        </td> 
+                    </tr> 
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     </section>
 </div>
