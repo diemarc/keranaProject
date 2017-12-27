7b05dbb4defdc7379f2f83bac6d96610f21d5351
diemarc <diemarc.os@gmail.com>
2017-12-27 05:32:13 +0100

    Modules refactor.

--- public/_layouts/default/_sidebar.php
+++ public/_layouts/default/_sidebar.php
@@ -1,41 +1,36 @@
-<div class="navbar-default sidebar" style="background-color: #ccc" role="navigation">
+<div class="navbar-default sidebar" style="" role="navigation">
     <div class="sidebar-nav navbar-collapse">
         <ul class="nav" id="side-menu">
 
             <li>
-                <a href="#"><i class="fa fa-dashboard fa-fw"></i> Home</a>
+                <a href="#"><i class="fa fa-dashboard fa-fw"></i>Dashboard</a>
             </li>
-            <?php if(__ENVIRONMENT__ == 'development') { ?>
             <li>
-                <a href="#"><i class="fa fa-wrench fa-fw"></i>System<span class="fa arrow"></span></a>
+                <a href="#"><i class="fa fa-user-md fa-fw"></i>System<span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li>
-                        <a href="<?php echo __URL__;?>/system/model/index">Models</a>
+                        <a href="<?php echo __URL__; ?>/system/user/index">Users</a>
                     </li>
-                    <li>
-                        <a href="<?php echo __URL__;?>/system/module/index">Modules</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo __URL__;?>/system/controller/index">Controllers</a>
-                    </li>
-
                 </ul>
                 <!-- /.nav-second-level -->
             </li>
+            <?php if (__ENVIRONMENT__ == 'development') { ?>
+                <li>
+                    <a href="#"><i class="fa fa-wrench fa-fw"></i>Development<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="<?php echo __URL__; ?>/system/module/index">Modules</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo __URL__; ?>/system/model/index">Models</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo __URL__; ?>/system/controller/index">Controllers</a>
+                        </li>
+                    </ul>
+                    <!-- /.nav-second-level -->
+                </li>
             <?php } ?>
-            <li>
-                <a href="#"><i class="fa fa-user-md fa-fw"></i>Usuarios<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="<?php echo __URL__;?>/system/user/index">Users</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo __URL__;?>/system/user/index">Groups</a>
-                    </li>
-
-                </ul>
-                <!-- /.nav-second-level -->
-            </li>
         </ul>
     </div>
     <!-- /.sidebar-collapse -->
