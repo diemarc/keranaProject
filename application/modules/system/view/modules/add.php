452eb1bca8eea6cac731c3af8c1c07a927bb75d9
diemarc <diemarc.os@gmail.com>
2017-12-27 05:44:52 +0100

    Modules refactor.

--- application/modules/system/view/modules/add.php
+++ application/modules/system/view/modules/add.php
@@ -1,23 +1,45 @@
 <div id="page-wrapper">
 
-    <h2> New module</h2>
-    <form action="<?php echo __URL__;?>/system/module/save" id="formKerana" name="formKerana" method="POST" class="form-horizontal"
-          accept-charset="utf-8">
-        <?php echo $kerana_token;?>
+    <h4 class='text-primary'> 
+        <i class="fa fa-list-alt fa-2x"></i>
+        <span class='text-muted'>apps</span>Modules->
+        <span class="text-success">AddModule</span>
+    </h4>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <form action="<?php echo __URL__; ?>/system/module/save" 
+                  id="formKerana" name="formKerana" method="POST" class="form-horizontal"
+                  accept-charset="utf-8">
+                      <?php echo $kerana_token; ?>
 
-        <div class="form-group">
-            <label for="f_module">Module</label>
-            <input type="text" class="form-control" id="f_module" 
-                   name="f_module" placeholder="nombre del modulo">
-        </div>
-        <div class="form-group">
-            <label for="f_sw_restricted">Access type</label>
-            <select name="f_sw_restricted" id="f_sw_restricted">
-                <option value='0'>Public</option>
-                <option value='1'>Only for authorized users</option>
-            </select>
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-    </form>
+                <header class="breadcrumb">
+                    <a href="<?php echo __URL__; ?>/system/module/index" 
+                       class="btn btn-warning">Cancel</a>
+                    <button type="submit" class="btn btn-success">Save</button>
+                </header>
 
+                <div class='form-group form-group-sm'> 
+                    <label for='f_module' class='col-sm-2 control-label'>ModuleName</label> 
+                    <div class='col-sm-4'> 
+                        <div class='input-group col-sm-4'> 
+                            <input type="text" id="f_module" name="f_module" 
+                                   class="form-control"  maxlength="20" required  />
+                        </div> 
+                    </div> 
+                </div> 
+                <div class='form-group form-group-sm'> 
+                    <label for='f_sw_restricted' class='col-sm-2 control-label'>AccessType</label> 
+                    <div class='col-sm-4'> 
+                        <div class='input-group col-sm-4'> 
+                            <select name="f_sw_restricted" class="form-control" 
+                                    id="f_sw_restricted">
+                                <option value='0'>Public</option>
+                                <option value='1'>Restricted</option>
+                            </select>
+                        </div> 
+                    </div> 
+                </div> 
+            </form>
+        </div>
+    </div>
 </div>
