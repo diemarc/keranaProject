ebc689123af9da15f5d1d58e665eb2a73bf6105e
diemarc <diemarc.os@gmail.com>
2017-12-27 06:14:26 +0100

    Modules refactor.

--- application/modules/system/controller/systemController.php
+++ application/modules/system/controller/systemController.php
@@ -3,20 +3,25 @@
 namespace application\modules\system\controller;
 
 defined('__APPFOLDER__') OR exit('Direct access to this file is forbidden, siya');
+
 /**
  * -----------------------------------------------------------------------------
  * systemController
  * ------------------------------------------------------------------------------
  * @author , all inherit controllers only will be executed if 'development' 
- * ENVIRONMENT is seted in index file. 
+ * ENVIRONMENT is setted in index file. 
  */
 class SystemController extends \kerana\Kerana
 {
+
     public function __construct()
     {
         parent::__construct();
-        
-        (__ENVIRONMENT__ != 'development') ? exit('Forbidden') : '';
-        
+
+        if (__ENVIRONMENT__ != 'development') {
+            http_response_code(403);
+            die('Forbbiden');
+        }
     }
+
 }
