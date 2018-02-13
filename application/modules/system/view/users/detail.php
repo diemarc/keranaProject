fe7985eeffdb35ca142f68b4f052ce16dd62a55b
diemarc <diemarc.os@gmail.com>
2018-02-13 07:44:53 +0100

    Add in confFile, to switch ACL function.

--- application/modules/system/view/users/detail.php
+++ application/modules/system/view/users/detail.php
@@ -12,45 +12,47 @@
         <div class="panel panel-info">
             <div class="panel-heading">
                 <a href='/system/user/edit/<?php echo $rsUser->id_user; ?>' 
-                   class='btn btn-default btn-xs' title='Edit'>
+                   class='btn btn-info btn-xs' title='Edit'>
                     <i class='fa fa-edit'></i>
                 </a> 
-                <span class="text-success">User details</span>
+                <a href='/system/user/edit/<?php echo $rsUser->id_user; ?>' 
+                   class='btn btn-info btn-xs' title='BlockUser'>
+                    <i class='fa fa-exclamation-circle'></i>
+                </a> 
             </div>
             <div class="panel-body">
                 <table class="table table-condensed table-bordered">
                     <thead class="small">
                         <tr>
-                            <td>UID</td>
+                            <td class="well well-sm">UID</td>
                             <td><?php echo $rsUser->id_user; ?></td>
                         </tr>
                         <tr>
-                            <td>Username</td>
+                            <td class="well well-sm">Username</td>
                             <td><?php echo $rsUser->username; ?></td>
                         </tr>
                         <tr>
-                            <td>Password</td>
+                            <td class="well well-sm">Password</td>
                             <td>
                                 <button class="btn btn-default btn-sm" type="button">
                                     Change
                                 </button>
-
                             </td>
                         </tr>
                         <tr>
-                            <td>Name</td>
+                            <td class="well well-sm">Name</td>
                             <td><?php echo $rsUser->name; ?></td>
                         </tr>
                         <tr>
-                            <td>Lastname</td>
+                            <td class="well well-sm">Lastname</td>
                             <td><?php echo $rsUser->lastname; ?></td>
                         </tr>
                         <tr>
-                            <td>Email</td>
+                            <td class="well well-sm">Email</td>
                             <td><?php echo $rsUser->email; ?></td>
                         </tr>
                         <tr>
-                            <td>Active?</td>
+                            <td class="well well-sm">Active?</td>
                             <td><?php echo $rsUser->sw_active; ?></td>
                         </tr>
                     </thead>
@@ -59,24 +61,23 @@
         </div>
 
     </div>
-    <div class="col-sm-6">
-        <div>
-
+    <div class="col-sm-8">
+        <div class="">
             <!-- Nav tabs -->
             <ul class="nav nav-tabs" role="tablist">
                 <li role="presentation" class="active">
                     <a href="#tab_groups" aria-controls="Groups" role="tab" data-toggle="tab">
-                        Groups
+                        <span class="text-muted">Groups</span>
                     </a>
                 </li>
                 <li role="presentation">
                     <a href="#tab_privileges" aria-controls="Privileges" role="tab" data-toggle="tab">
-                        Privileges (MCA)
+                        <span class="text-success">Privileges (MCA)</span>
                     </a>
                 </li>
                 <li role="presentation">
                     <a href="#tab_badlogins" aria-controls="BadLogin" role="tab" data-toggle="tab">
-                        BadLogin
+                        <span class="text-danger">BadLogin</span>
                     </a>
                 </li>
                 <li role="presentation">
@@ -88,12 +89,40 @@
 
             <!-- Tab panes -->
             <div class="tab-content">
-                <div role="tabpanel" class="tab-pane active" id="tab_groups">...</div>
-                <div role="tabpanel" class="tab-pane" id="tab_privileges">Priveleges</div>
-                <div role="tabpanel" class="tab-pane" id="tab_badlogins">BL</div>
-                <div role="tabpanel" class="tab-pane" id="tab_settings">...</div>
-            </div>
+                <div role="tabpanel" class="tab-pane active" id="tab_groups">
 
+                </div>
+                <div role="tabpanel" class="tab-pane" id="tab_privileges">
+                    <div class="panel panel-green">
+                        <div class="panel-body">
+                            <div class="breadcrumb">
+                                <a href='/system/user/edit/<?php echo $rsUser->id_user; ?>' 
+                                   class='btn btn-success btn-xs' title='AddMca'>
+                                    <i class='fa fa-plus'></i>
+                                </a> 
+                            </div>
+                            <div class="table-responsive">
+                                <table class="table table-condensed table-bordered">
+                                    <thead class="bg-success">
+                                        <tr>
+                                            <th>Module</th>
+                                            <th>Controller</th>
+                                            <th>Action</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div role="tabpanel" class="tab-pane" id="tab_badlogins">
+
+                </div>
+                <div role="tabpanel" class="tab-pane" id="tab_settings">
+
+                </div>
+            </div>
         </div>
     </div>
 </div>
