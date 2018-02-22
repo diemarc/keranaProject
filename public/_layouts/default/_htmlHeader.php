4197f8a326d0daf272f0b63b348e58f8618cd60e
diemarc <diemarc.os@gmail.com>
2018-02-22 04:40:11 +0100

    Refactoring Layouts.

--- public/_layouts/default/_htmlHeader.php
+++ public/_layouts/default/_htmlHeader.php
@@ -17,20 +17,14 @@
         <link href="/_styles/sb_admin/dist/css/sb-admin-2.css" rel="stylesheet">
         <link href="/_styles/sb_admin/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <link href="/_styles/sb_admin/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-       
+       <script src="<?php echo __URL__.'/_styles/sb_admin/vendor/jquery/jquery.min.js';?>"></script>
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
-        <script>
-          
 
-
-
-
-        </script>
     </head>
     <body>
         <div id="wrapper">
