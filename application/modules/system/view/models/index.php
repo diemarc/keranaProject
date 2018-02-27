f8b0468c3d5b89714f9fc61ad88c2d608d6db108
diemarc <diemarc.os@gmail.com>
2018-02-27 06:35:44 +0100

    Protect system models.

--- application/modules/system/view/models/index.php
+++ application/modules/system/view/models/index.php
@@ -1,12 +1,12 @@
 <div id="page-wrapper">
-     <h4 class='text-primary'> 
+    <h4 class='text-primary'> 
         <i class="fa fa-database fa-2x"></i>
         <span class='text-muted'>apps</span>Models
     </h4>
     <header class="breadcrumb">
-        <a href="<?php echo __URL__;?>/system/model/add" 
+        <a href="<?php echo __URL__; ?>/system/model/add" 
            class="btn btn-success">
-        <i class='fa fa-plus'></i> add
+            <i class='fa fa-plus'></i> add
         </a>
     </header>
     <section id='section_modelos' class='table-responsive'>
@@ -21,24 +21,27 @@
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($rsModelos AS $modelo) : ?>
+                <?php foreach ($rsModels AS $model) : ?>
                     <tr>
-                        <td><?php echo $modelo->id_model;?></td>
-                        <td><?php echo $modelo->model;?></td>
-                        <td><?php echo $modelo->table_reference;?></td>
-                        <td><?php echo $modelo->model_description;?></td>
+                        <td><?php echo $model->id_model; ?></td>
+                        <td><?php echo $model->model; ?></td>
+                        <td><?php echo $model->table_reference; ?></td>
+                        <td><?php echo $model->model_description; ?></td>
                         <td class="well">
-                            <a href='<?php echo __URL__;?>/system/model/detail/<?php echo $modelo->id_model; ?>' 
+                            <a href='<?php echo __URL__; ?>/system/model/detail/<?php echo $model->id_model; ?>' 
                                class='btn btn-default btn-xs'>
                                 <i class='fa fa-eye'></i>
                             </a>
-                            <a href='' class='btn btn-info btn-xs'>
-                                <i class='fa fa-pencil'></i>
-                            </a>
-                            <a href='<?php echo __URL__;?>/system/model/delete/<?php echo $modelo->id_model;?>' 
-                               class='btn btn-danger btn-xs'>
-                                <i class='fa fa-trash-o'></i>
-                            </a>
+                            <?php if ($model->is_system_model == 0) { ?>
+                                <a href='' class='btn btn-info btn-xs'>
+                                    <i class='fa fa-pencil'></i>
+                                </a>
+
+                                <a href='<?php echo __URL__; ?>/system/model/delete/<?php echo $model->id_model; ?>' 
+                                   class='btn btn-danger btn-xs'>
+                                    <i class='fa fa-trash-o'></i>
+                                </a>
+                            <?php } ?>
                         </td>
                     </tr>
                 <?php endforeach; ?>
