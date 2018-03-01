841bf74d2ef413b793bbd3d2026683c1b40dcc5c
diemarc <diemarc.os@gmail.com>
2018-03-01 05:53:46 +0100

    View refactor.

--- application/modules/welcome/controller/loginController.php
+++ application/modules/welcome/controller/loginController.php
@@ -29,7 +29,7 @@ class LoginController  // minu\may
      */
     public function introduceMySelf()
     {
-        \kerana\View::showView('welcome', 'login', false, false, false);
+        \kerana\View::showLoginPage();
     }
 
     /*
