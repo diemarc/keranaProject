5c022f4507161d9df6580f497212b000a9e88b5a
diemarc <diemarc.os@gmail.com>
2018-03-14 05:46:07 +0100

    Layout for fac2fast

---
+++ public/_layouts/fac2fast/_nav.php
@@ -0,0 +1,38 @@
+<!-- Navigation -->
+<nav class="navbar navbar-static-top bg-success" role="navigation" style="margin-bottom: 0">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="">
+            <span class="pull-left text-success">
+                <?php echo __APPNAME__; ?>
+            </span>
+            <span class="small">
+                <span class="small"></span>
+            </span>
+
+        </a>
+    </div>
+    <!-- /.navbar-header -->
+
+    <ul class="nav navbar-top-links navbar-right">
+        <!-- /.dropdown -->
+        <li class="dropdown">
+            <a class="dropdown-toggle" href="<?php echo __URL__; ?>/welcome/login/closeSession">
+                <?php echo $_SESSION['username']; ?> <i class="glyphicon glyphicon-log-out"></i>
+            </a>
+            <!-- /.dropdown-tasks -->
+        </li>
+        <!-- /.dropdown -->
+        <!-- /.dropdown -->
+        <!-- /.dropdown -->
+    </ul>
+    <!-- /.navbar-top-links -->
+    <?php include(__DOCUMENTROOT__ . '/_layouts/'.$_SESSION['layout'].'/_sidebar.php'); ?>
+
+    <!-- /.navbar-static-side -->
+</nav>
