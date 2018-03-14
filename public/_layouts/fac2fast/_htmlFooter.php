82a389abc75d40ee767167ca7f64d4cc1ade53bc
diemarc <diemarc.os@gmail.com>
2018-03-14 05:46:39 +0100

    Layout for fac2fast

---
+++ public/_layouts/fac2fast/_htmlFooter.php
@@ -0,0 +1,34 @@
+
+</div>
+<!-- /#wrapper -->
+ 
+<div class='modal' style="overflow-x:hidden; overflow-y:scroll;" style="" id="div_aux2"></div> 
+<div class='modal' style="overflow-x:hidden; overflow-y:scroll;" style="" id="div_aux3"></div> 
+
+<!-- jQuery -->
+<script src="<?php echo __URL__.'/_styles/sb_admin/vendor/jquery/jquery.min.js';?>"></script>
+<!--<script src="/src/js/tab.js" type="text/javascript" language="javascript"></script>
+<script src="/src/js/tooltip.js" type="text/javascript" language="javascript"></script>
+<script src="/src/js/popover.js" type="text/javascript" language="javascript"></script>
+<script src="/src/js/bootstrap-dropdown.js" type="text/javascript" language="javascript"></script>
+<script src="/src/js/bootstrap-modal.js" type="text/javascript" language="javascript"></script>
+<script src="/src/js/bootstrap-collapse.js" type="text/javascript" language="javascript"></script>-->
+
+<!-- Bootstrap Core JavaScript -->
+<script src="/_styles/sb_admin/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="/_styles/sb_admin/vendor/metisMenu/metisMenu.min.js"></script>
+
+<!-- Morris Charts JavaScript -->
+
+<!-- Custom Theme JavaScript -->
+<script src="/_styles/sb_admin/dist/js/sb-admin-2.js"></script>
+
+<script src="<?php echo __URL__.'/src/js/kerana.js';?>"></script>
+
+</body>
+
+</html>
+
+
