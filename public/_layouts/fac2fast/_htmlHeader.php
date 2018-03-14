793bcd5ba615a480f2ec86301687c77f83ba12e7
diemarc <diemarc.os@gmail.com>
2018-03-14 05:46:14 +0100

    Layout for fac2fast

---
+++ public/_layouts/fac2fast/_htmlHeader.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title><?php echo __APPNAME__ ?></title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="/_styles/sb_admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/_styles/sb_admin/vendor/metisMenu/metisMenu.css" rel="stylesheet">
+        <link href="/_styles/sb_admin/dist/css/sb-admin-2.css" rel="stylesheet">
+        <link href="/_styles/sb_admin/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="/_styles/sb_admin/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+    <body>
+        <div id="wrapper">
+            <?php
+            include(__DOCUMENTROOT__ . '/_layouts/'.$_SESSION['layout'].'/_nav.php');
+            
