560317cbef15f24119fd65a363176deefdb33a8a
diemarc <diemarc.os@gmail.com>
2018-03-14 05:46:11 +0100

    Layout for fac2fast

---
+++ public/_layouts/fac2fast/_sidebar.php
@@ -0,0 +1,40 @@
+<div class="navbar-default sidebar" style="" role="navigation">
+    <div class="sidebar-search">
+        <div class="form-group form-group-sm has-success has-feedback">
+            <div class="input-group">
+                <input type="text" id="param_find" class="form-control input-sm" 
+                       placeholder="Encuentra algo"
+                       onchange="findString(this.value)"/>
+                <span class="input-group-addon">
+                    <i class="fa fa-search"></i>
+                </span>
+            </div>
+
+            <div id="div_resultado" class="hidden">
+                <ul class="list-group" id="div_find"></ul>
+            </div> 
+            <!-- /input-group -->
+        </div>
+    </div>
+    <div class="sidebar-nav navbar-collapse">
+        <ul class="nav" id="side-menu">
+
+            <li>
+                <a href=""><i class="fa fa-dashboard fa-fw"></i>Dashboard</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-user-md fa-fw"></i>System<span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="<?php echo __URL__; ?>/system/user/index">Users</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo __URL__; ?>/system/group/index">Groups</a>
+                    </li>
+                </ul>
+                <!-- /.nav-second-level -->
+            </li>
+        </ul>
+    </div>
+    <!-- /.sidebar-collapse -->
+</div>
