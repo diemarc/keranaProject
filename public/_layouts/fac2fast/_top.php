2f2217f7a7f5bb07681cbbdc8ba9fa0ccff33205
diemarc <diemarc.os@gmail.com>
2018-03-15 07:29:09 +0100

    f2f controller models and modules.

--- public/_layouts/fac2fast/_top.php
+++ public/_layouts/fac2fast/_top.php
@@ -1,37 +1,29 @@
 <ul class="navbar-nav ml-auto">
     <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa fa-fw fa-envelope"></i>
-            <span class="d-lg-none">Messages
-                <span class="badge badge-pill badge-primary">12 New</span>
-            </span>
-            <span class="indicator text-primary d-none d-lg-block">
-                <i class="fa fa-fw fa-circle"></i>
-            </span>
+           <?php 
+            if(isset($_SESSION['f2f_id_contratante'])) {
+                echo '<strong>'.$_SESSION['f2f_contratante'].'</strong>';
+            }
+           
+           ?>
         </a>
+        <?php if(isset($_SESSION['f2f_contratantes_array']) AND !empty($_SESSION['f2f_contratantes_array'])){ ?>
         <div class="dropdown-menu" aria-labelledby="messagesDropdown">
-            <h6 class="dropdown-header">New Messages:</h6>
+            <h6 class="dropdown-header">Cambiar de empresa:</h6>
+            
+            <?php foreach($_SESSION['f2f_contratantes_array'] AS $contra):?>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-                <strong>David Miller</strong>
-                <span class="small float-right text-muted">11:21 AM</span>
-                <div class="dropdown-message small">Hey there! This new version of SB Admin is pretty awesome! These messages clip off when they reach the end of the box so they don't overflow over to the sides!</div>
+            <a class="dropdown-item" href="<?php echo __URL__.'/fac2fast/f2f/changeCompany/'.$contra['id_contratante'];?>">
+                <strong><?php echo $contra['contratante'];?></strong>
+                <span class="small float-right text-muted"><?php echo $contra['cif'];?></span>
+                <div class="dropdown-message small">
+                    <?php echo $contra['nombre_contra'];?>
+                </div>
             </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-                <strong>Jane Smith</strong>
-                <span class="small float-right text-muted">11:21 AM</span>
-                <div class="dropdown-message small">I was wondering if you could meet for an appointment at 3:00 instead of 4:00. Thanks!</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-                <strong>John Doe</strong>
-                <span class="small float-right text-muted">11:21 AM</span>
-                <div class="dropdown-message small">I've sent the final files over to you for review. When you're able to sign off of them let me know and we can discuss distribution.</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item small" href="#">View all messages</a>
+            <?php endforeach;?>
         </div>
+        <?php } ?>
     </li>
     <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
