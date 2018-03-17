85f77004c1fb78e2bacb069414667e85aaf5c64f
diemarc <diemarc.os@gmail.com>
2018-03-17 09:34:26 +0100

    view refactor

---
+++ public/_layouts/f2f/_sidebar.php
@@ -0,0 +1,35 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <i class="fa fa-user-circle"></i>
+            </div>
+            <div class="pull-left info">
+                <p><?php echo $_SESSION['username']; ?></p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Buscar...">
+                <span class="input-group-btn">
+                    <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                    </button>
+                </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">NAVEGACI&Oacute;N PRINCIPAL</li>
+            <li><a href="<?php echo __URL__.'/fac2fast/f2f/index';?>"><i class="fa fa-dashboard"></i> <span>Home</span></a></li>
+            <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+            <li class="header">SOPORTE</li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
