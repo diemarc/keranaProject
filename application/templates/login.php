5e04877e52767338e0f44ef1deda6fb3088a1f1d
diemarc <diemarc.os@gmail.com>
2018-03-19 16:26:19 +0100

    view login refactor

--- application/templates/login.php
+++ application/templates/login.php
@@ -58,7 +58,7 @@
                     </div>
                     <div class="row">
                         <!-- /.col -->
-                        <div class="col-xs-6">
+                        <div class="col-xs-6 col-lg-offset-3">
                             <button type="submit" class="btn btn-primary btn-block btn-flat">Conectarme</button>
                         </div>
                         <!-- /.col -->
