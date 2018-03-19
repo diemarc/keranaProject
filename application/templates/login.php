f6e96787367ed9311d6125dbe49c9b2611de0ce3
diemarc <diemarc.os@gmail.com>
2018-03-19 16:09:08 +0100

    view login refactor

--- application/templates/login.php
+++ application/templates/login.php
@@ -1,41 +1,94 @@
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-15" />
-        <meta name="lang" content="es" />
-        <meta name="author" content="diemarc" />
-        <meta name="organization" content="diemarc" />
-        <meta name="locality" content="Madrid" />
-        <title><?php echo __APPNAME__;?>-Login</title>
-        <!-- Bootstrap Core CSS -->
-        <link href="/_styles/sb_admin2/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    </head>
-    <body>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title><?php echo __APPNAME__; ?></title>
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <!-- Bootstrap 3.3.7 -->
+        <link rel="stylesheet" href="/_layouts/f2f/bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="/_layouts/f2f/bower_components/font-awesome/css/font-awesome.min.css">
+        <!-- Ionicons -->
+        <link rel="stylesheet" href="/_layouts/f2f/bower_components/Ionicons/css/ionicons.min.css">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="/_layouts/f2f/dist/css/AdminLTE.min.css">
+        <!-- AdminLTE Skins. Choose a skin from the css/skins
+             folder instead of downloading all of them to reduce the load. -->
+        <link rel="stylesheet" href="/_layouts/f2f/dist/css/skins/_all-skins.min.css">
+        <!-- Morris chart -->
+        <link rel="stylesheet" href="/_layouts/f2f/bower_components/morris.js/morris.css">
+        <!-- jvectormap -->
+        <link rel="stylesheet" href="/_layouts/f2f/bower_components/jvectormap/jquery-jvectormap.css">
+        <!-- Date Picker -->
+        <link rel="stylesheet" href="/_layouts/f2f/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+        <!-- Daterange picker -->
+        <link rel="stylesheet" href="/_layouts/f2f/bower_components/bootstrap-daterangepicker/daterangepicker.css">
+        <!-- bootstrap wysihtml5 - text editor -->
+        <link rel="stylesheet" href="/_layouts/f2f/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
 
-        <div class="container">
-            <div class="col-xs-10 col-sm-4 col-md-4 col-md-offset-4">
-                <div class="panel panel-success">
-                    <div class="panel-body">
-                        <form class="form-signin form-group-sm" 
-                              action="go" method="post">
-                            <h2 class="form-signin-heading text-primary"><?php echo __APPNAME__;?></h2>
-                            <label for="f_username" class="sr-only">Username? </label>
-                            <input type="text" id="f_username" name="f_username" class="form-control form-group-sm" 
-                                   placeholder="username? " required autofocus>
-                            <label for="f_password" class="sr-only">Password.?</label>
-                            <input type="password" id="f_password" name="f_password" class="form-control" placeholder="Password?" required>
-                           <hr/>
-                            <button class="btn btn-lg btn-success btn-block" type="submit">OK</button>
-                        </form>
-                        <?php if (!empty($error)) { ?>
-                            <div class="alert alert-danger" role="alert">
-                                <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                                <span class="sr-only">Error:</span>
-                                <?php echo $error; ?>
-                            </div>
-                        <?php } ?>
-                    </div>
-                </div>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- Google Font -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    </head>
+
+    <body class="hold-transition login-page">
+        <div class="login-box">
+            <div class="login-logo">
+                <a href="<?php echo __URL__; ?>"><b>fac</b>2Fast</a>
             </div>
-        </div> <!-- /container -->
+            <!-- /.login-logo -->
+            <div class="login-box-body">
+                <p class="login-box-msg">Inicie sesi&oacute;n</p>
+
+                <form action="go" method="post">
+                    <div class="form-group has-feedback">
+                        <input type="text" name="f_username" id="f_username" class="form-control" placeholder="Usuario">
+                        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                    </div>
+                    <div class="form-group has-feedback">
+                        <input type="password" name="f_password" id="f_password" class="form-control" placeholder="Password">
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
+                    <div class="row">
+                        <!-- /.col -->
+                        <div class="col-xs-6">
+                            <button type="submit" class="btn btn-primary btn-block btn-flat">Conectarme</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+                <!-- /.social-auth-links -->
+
+                <a href="#">Olvid&eacute; mi contrase&ntilde;a</a><br>
+                <a href="#" class="text-center">Registrarme a f2f</a>
+
+            </div>
+            <!-- /.login-box-body -->
+        </div>
+        <!-- /.login-box -->
+
+        <!-- jQuery 3 -->
+        <script src="/_layouts/f2f/bower_components/jquery/dist/jquery.min.js"></script>
+        <!-- Bootstrap 3.3.7 -->
+        <script src="/_layouts/f2f/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <!-- iCheck -->
+        <script src="/_layouts/f2f/plugins/iCheck/icheck.min.js"></script>
+        <script>
+            $(function () {
+                $('input').iCheck({
+                    checkboxClass: 'icheckbox_square-blue',
+                    radioClass: 'iradio_square-blue',
+                    increaseArea: '20%' // optional
+                });
+            });
+        </script>
     </body>
 </html>
